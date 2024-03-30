60ec1062f49335da619f4760612d191ebd6a838d
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-03-30 22:00:33 +0530

    main

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -10,6 +10,8 @@ const Resume = () => {
   <div className="resume-content">
     <img src={resume}>
     </img>
+
+    
     <a href='https://drive.google.com/file/d/1GeLJbZpARHhxizvxzaBCFf_nRqheW9sB/view?usp=drive_link'><button>View Resume</button></a>
   </div>
   </div>
